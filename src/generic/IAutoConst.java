175bd6ef2373ba5350cb9e0782ddce08cd880dfa
komaljha <35664206+komaljha@users.noreply.github.com>
2018-03-10 21:04:52 +0530

    code commented for XL code

--- src/generic/IAutoConst.java
+++ src/generic/IAutoConst.java
@@ -4,7 +4,7 @@ public interface IAutoConst {
 	String CHROME_KEY="webdriver.chrome.driver";
 	String CHROME_VALUE="./driver/chromedriver.exe";
 	
-	String XLPATH="./data/input.xlsx";
+	//String XLPATH="./data/input.xlsx";
 }
 
 
